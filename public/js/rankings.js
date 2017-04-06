5795436d7f4b5ffb7ac5b0da2b45f3f7e170edec
Kenny Tang <kennyftang@gmail.com>
2017-04-05 21:37:48 -0500

    Database connection working

--- public/js/rankings.js
+++ public/js/rankings.js
@@ -1,47 +1,67 @@
 /**
  * Created by kenyunot on 27/01/2017.
  */
+ var removing = false;
 function addPlayer() {
-    //Form for the input
-    var nameForm = document.createElement("form");
-    //Input for entering name
-    var nameInput = document.createElement("input");
-    //Button for submitting name
-    var submitFormButton = document.createElement("button");
-    //Row to be inserted to table
-    var newRow = document.createElement("tr");
-    //td for the input
-    var tdInput = document.createElement("td");
-    //td for the button
-    var tdButton = document.createElement("td");
-    nameForm.setAttribute("id", "nameform");
-    nameForm.setAttribute("method", "post");
-    nameForm.appendChild(nameInput);
-    nameInput.setAttribute("type", "text");
-    nameInput.setAttribute("name", "name");
-    submitFormButton.setAttribute("type", "submit");
-    submitFormButton.setAttribute("form", "nameform");
-    submitFormButton.innerText = "Add";
-    newRow.appendChild(document.createElement("td"));
-    newRow.appendChild(tdInput);
-    newRow.appendChild(tdButton);
-    newRow.appendChild(document.createElement("td"));
-    newRow.setAttribute("id", "newplayer");
-    tdButton.appendChild(submitFormButton);
-    tdInput.appendChild(nameForm);
-    //Insert the new row into the table
-    document.getElementsByTagName("tbody").item(0).appendChild(newRow);
-    //
-    var newPlayerForm = document.createElement("form");
-    newPlayerForm.setAttribute("method", "post");
-    //TODO: Wrap this button around a form to POST the name
-    var button = document.getElementsByClassName("add")[0];
-    button.parentNode.removeChild(button);
+  //Form for the input
+  var nameForm = document.createElement('form');
+  //Input for entering name
+  var nameInput = document.createElement('input');
+  //Button for submitting name
+  var submitFormButton = document.createElement('button');
+  //Row to be inserted to table
+  var newRow = document.createElement('tr');
+  //td for the input
+  var tdInput = document.createElement('td');
+  //td for the button
+  var tdButton = document.createElement('td');
+
+  nameForm.setAttribute('id', 'nameform');
+  nameForm.setAttribute('method', 'post');
+  nameForm.appendChild(nameInput);
+
+  nameInput.setAttribute('type', 'text');
+  nameInput.setAttribute('name', 'addname');
+
+  submitFormButton.setAttribute('type', 'submit');
+  submitFormButton.setAttribute('form', 'nameform');
+  submitFormButton.innerText = 'Add';
+
+  newRow.appendChild(document.createElement('td'));
+  newRow.appendChild(tdInput);
+  newRow.appendChild(tdButton);
+  newRow.appendChild(document.createElement('td'));
+  newRow.setAttribute('id', 'newplayer');
+
+  tdButton.appendChild(submitFormButton);
+  tdInput.appendChild(nameForm);
+  //Insert the new row into the table
+  document.getElementsByTagName('table').item(0).appendChild(newRow);
+  var newPlayerForm = document.createElement('form');
+  newPlayerForm.setAttribute('method', 'post');
+  var button = document.getElementsByClassName('add')[0];
+  button.parentNode.removeChild(button);
 }
 function removePlayer() {
-    var person = window.prompt("Rank #");
-    var removeButton = document.getElementsByClassName("remove").item(0);
-    removeButton.setAttribute("name", "removerank");
-    removeButton.setAttribute("value", person);
-    removeButton.setAttribute("type", "submit");
+  if(!removing) {
+    var removeHead = document.createElement('th');
+    removeHead.innerText = 'Remove';
+    document.getElementsByClassName('head').item(0).appendChild(removeHead);
+    var players = document.getElementsByClassName('player');
+    for(var i = 0; i < players.length; i++) {
+      var element = players.item(i);
+      var tdRemove = document.createElement('td');
+      var removeForm = document.createElement('form');
+      var removeButton = document.createElement('button');
+      removeForm.setAttribute('method', 'post');
+      console.log(element.children.item(1).innerText);
+      removeButton.setAttribute('name', 'removename');
+      removeButton.setAttribute('value', element.children.item(1).innerText);
+      removeButton.innerText = 'Remove';
+      removeForm.appendChild(removeButton);
+      tdRemove.appendChild(removeForm);
+      element.appendChild(tdRemove);
+    }
+    removing = true;
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,49 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('sql/players.sqlite3');
 const pug = require('pug');
 
-
 db.serialize(function() {
-  db.run('CREATE TABLE IF NOT EXISTS "players" ("name" TEXT NOT NULL, "elo" INT NOT NULL, "hasChange" BOOLEAN NOT NULL DEFAULT FALSE, "change" BOOLEAN)');
-});
-
-var players = {};
-db.all('SELECT * FROM "players"', {}, function(e, val) {
-  players = val;
+  db.run('CREATE TABLE IF NOT EXISTS "players" ("name" TEXT NOT NULL UNIQUE, "elo" INT NOT NULL DEFAULT 1200, "hasChange" BOOLEAN NOT NULL DEFAULT FALSE, "change" BOOLEAN)');
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('scores', {players: players, isLoggedIn: true, user: 'kenny' });
+  var players = {};
+  db.all('SELECT * FROM "players"', {}, function(e, val) {
+    players = val;
+    res.render('scores', {players: players, isLoggedIn: true, user: 'kenny' });
+  });
 });
 
-/*POST home page*/
+/* POST home page*/
 router.post('/', function(req, res, next) {
-  res.render('scores', {players: players, isLoggedIn: true, user: 'kenny' });
+  var post = req.body;
+  var error;
+  console.log(post);
+  if(post.addname != null) {
+    db.serialize(function() {
+      db.run('INSERT INTO "players" ("name") VALUES (?)', post.addname, function(e) {
+        //Error Occured
+        if(e != null) {
+          if(e.errno == 19)
+            error = 'Name already exists!';
+        }
+      });
+    });
+    console.log('New player ' + post.name + ' added.');
+  }
+  if(post.removename != null) {
+    db.serialize(function() {
+      db.run('DELETE FROM "players" WHERE "name" = ?', post.removename);
+    });
+  }
+  var players = {};
+  db.all('SELECT * FROM "players"', {}, function(e, val) {
+    players = val;
+    if(typeof(error) != 'undefined')
+      res.render('scores', {players: players, isLoggedIn: true, user: 'kenny', error: error });
+    else
+      res.render('scores', {players: players, isLoggedIn: true, user: 'kenny' });
+  });
 });
 
 module.exports = router;
